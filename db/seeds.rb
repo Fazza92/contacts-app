85d0bebe4c092f5f2ea11940b10bc16f3aad3b20
Oscar <oscarwasil@gmail.com>
2018-05-31 17:57:54 -0500

    Added features

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,21 @@
 
 
-100.times do 
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
+# 100.times do 
+#   first_name = Faker::Name.first_name
+#   last_name = Faker::Name.last_name
 
-  Contact.create(
-                first_name: first_name,
-                last_name: last_name,
-                email: Faker::Internet.free_email("#{first_name}.#{last_name}"),
-                phone_number: Faker::PhoneNumber.phone_number
-                )
-end
+#   Contact.create(
+#                 first_name: first_name,
+#                 last_name: last_name,
+#                 email: Faker::Internet.free_email("#{first_name}.#{last_name}"),
+#                 phone_number: Faker::PhoneNumber.phone_number,
+#                 user_id: 1
+#                 )
+
+# end
+
+Category.create(name: "Cheap")
+Category.create(name: "Expensive")
+Category.create(name: "Wearable")
+Category.create(name: "Damaged")
+Category.create(name: "Home Good")
